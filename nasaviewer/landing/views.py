049aed36ad2b05699c7ee2bcd64240bed7d97568
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-04-29 10:08:22 -0500

    First display on index

--- nasaviewer/landing/views.py
+++ nasaviewer/landing/views.py
@@ -7,7 +7,6 @@ import requests
 def index(request):
 
     values = {'start_date':'2017-04-27','end_date':'2017-04-27','api_key':'hTcesBBBIuHHXsLgRxN4T29W2q4Db5DBE9jwr3bQ'}
-    nasa_api = requests.get('http://api.nasa.gov/neo/rest/v1/feed',params=values)
+    nasa_api = requests.get('https://api.nasa.gov/neo/rest/v1/feed',params=values)
     asteroids = nasa_api.json()
-    print(asteroids['near_earth_objects']['2017-04-26']['links'])
     return render(request, 'landing/index.html',{'asteroids':asteroids})
