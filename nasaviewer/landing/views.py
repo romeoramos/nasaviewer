d39ac85d65186a092318a8cf752ef2e6123b770d
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-02 20:10:52 -0500

    Final commit

--- nasaviewer/landing/views.py
+++ nasaviewer/landing/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from datetime import date, timedelta
 
 # Create your views here.
 
 def index(request):
-
-    values = {'start_date':'2017-04-27','end_date':'2017-04-27','api_key':'hTcesBBBIuHHXsLgRxN4T29W2q4Db5DBE9jwr3bQ'}
+    yesterday = str(date.today() - timedelta(days=1))
+    values = {'start_date':yesterday,'end_date':yesterday,'api_key':'hTcesBBBIuHHXsLgRxN4T29W2q4Db5DBE9jwr3bQ'}
     nasa_api = requests.get('https://api.nasa.gov/neo/rest/v1/feed',params=values)
-    asteroids = nasa_api.json()
-    return render(request, 'landing/index.html',{'asteroids':asteroids})
+    nasa_json = nasa_api.json()
+    asteroids = nasa_json['near_earth_objects'][yesterday]
+    return render(request, 'landing/index.html',{'asteroids':asteroids,'yesterday':yesterday})
